3b4f4268ec463200b40c3cc3ae384953dc278a98
mathengejoan <mathengejoan971@gmail.com>
2019-05-07 14:55:01 +0300

    today

---
+++ new/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Member
+from django.views.generic import ListView,CreateView,UpdateView, DeleteView
+# Create your views here.
+
+
+class MemberListView(ListView):
+    model = Member
+    template_name = 'new/index.html'
+    context_object_name = 'profiles'
+
+
+class MemberCreateView(CreateView):
+    model = Member
+    fields = '__all__'
+
+
+class MemberUpdateView(UpdateView):
+    model = Member
+    fields = '__all__'
+
+class MemberDeleteView(DeleteView):
+    model = Member
+    success_url = '/'
